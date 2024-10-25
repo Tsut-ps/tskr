f69ab485f4b2281d362d26fd78f28fda955bccf5
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2024-10-26 00:58:13 +0900

    Add: 今月の日付配列をタイムラインに配置

--- src/app/charts/page.tsx
+++ src/app/charts/page.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import { Calendar, ChevronLeft, ChevronRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 
 export default function Page() {
   const currentDate = new Date();
@@ -13,8 +14,15 @@ export default function Page() {
     return new Date(currentYear, currentMonthIndex + 1, 0).getDate(); // 翌月の0日 = 当月の最終日
   };
 
+  // 日付の配列を生成
+  const daysArray = () => {
+    const daysInMonth = getDaysInMonth();
+    const days = [...Array(daysInMonth)].map((_, i) => i + 1);
+    return days;
+  };
+
   return (
-    <main className="flex gap-4 p-4 md:gap-6 md:py-8 overflow-x-auto w-full">
+    <main className="flex flex-col gap-4 p-4 md:gap-6 md:py-8 overflow-x-auto w-full">
       <div className="flex justify-between w-full">
         <h2 className="mt-10 scroll-m-20 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
           タイムライン
@@ -38,6 +46,27 @@ export default function Page() {
           </Button>
         </div>
       </div>
+      <Card className="w-full">
+        <CardContent className="p-0">
+          <div className="relative">
+            <div className="grid grid-flow-col overflow-x-auto">
+              {daysArray().map((day) => (
+                <div
+                  key={day}
+                  className="flex items-center justify-center text-xs w-8 h-8"
+                >
+                  {day}
+                </div>
+              ))}
+            </div>
+            <div className="relative h-8">
+              <div>
+                <div>ここにコンテンツ</div>
+              </div>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
     </main>
   );
 }
