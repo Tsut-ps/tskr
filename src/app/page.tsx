c18a7e9753d5bf719515ad5abc49bcbfff057f21
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2024-11-09 07:09:57 +0900

    Add: Supabaseの導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-export default function Page() {
+import { createClient } from "@/utils/supabase/server";
+
+export default async function Page() {
+  const supabase = await createClient();
+  const { data: health } = await supabase.from("health").select();
+
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 md:gap-6 md:py-8 max-w-screen-xl">
-      index
+      {JSON.stringify(health, null, 2)}
     </main>
   );
 }
