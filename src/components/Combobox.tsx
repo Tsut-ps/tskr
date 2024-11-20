e32526c21fd211e7802cfef1a88d3defaea86ea0
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2024-11-20 20:37:56 +0900

    Update: Comboboxでプロジェクトの切り替えができるように変更

--- src/components/Combobox.tsx
+++ src/components/Combobox.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import * as React from "react";
-import { Check, ChevronsUpDown } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { useState, useMemo } from "react";
+import { Check, ChevronsUpDown, Plus } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
@@ -19,20 +20,30 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 
-const frameworks = [
+type StoreProject = {
+  slug: string;
+  name: string;
+};
+
+const storeProjects: StoreProject[] = [
   {
-    value: "project-a",
-    label: "プロジェクトA",
-  },
-  {
-    value: "project-b",
-    label: "プロジェクトB",
+    slug: "00000000-0000-0000-0000-000000000000",
+    name: "(dummy)",
   },
 ];
 
 export function Combobox() {
-  const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState("");
+  const { projectSlug } = useParams();
+  const router = useRouter();
+  const [open, setOpen] = useState(false);
+  const [selectedSlug, setSelectedSlug] = useState(projectSlug);
+
+  // 選択中のプロジェクトを取得 (遷移時に再取得しない)
+  const selectedProject = useMemo(
+    () =>
+      storeProjects.find((storeProject) => storeProject.slug === selectedSlug),
+    [selectedSlug]
+  );
 
   return (
     <div className="hidden md:flex">
@@ -44,36 +55,50 @@ export function Combobox() {
             aria-expanded={open}
             className="md:w-[180px] lg:w-[240px] justify-between"
           >
-            {value
-              ? frameworks.find((framework) => framework.value === value)?.label
-              : "プロジェクトを選択"}
+            {selectedProject?.name ?? "プロジェクトを選択"}
             <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="w-[200px] p-0">
+        <PopoverContent className="w-[240px] p-0">
           <Command>
-            <CommandInput placeholder="Search framework..." />
+            <CommandInput placeholder="プロジェクトを探す" />
             <CommandList>
-              <CommandEmpty>No framework found.</CommandEmpty>
-              <CommandGroup>
-                {frameworks.map((framework) => (
+              <CommandEmpty>プロジェクトが見つかりません。</CommandEmpty>
+              <CommandGroup heading="アクセス履歴一覧">
+                {storeProjects.map((project) => (
                   <CommandItem
-                    key={framework.value}
-                    value={framework.value}
-                    onSelect={(currentValue) => {
-                      setValue(currentValue === value ? "" : currentValue);
+                    key={project.slug}
+                    value={project.name}
+                    onSelect={() => {
+                      setSelectedSlug(project.slug);
                       setOpen(false);
+                      router.push(`/p/${project.slug}`);
                     }}
                   >
                     <Check
                       className={cn(
                         "mr-2 h-4 w-4",
-                        value === framework.value ? "opacity-100" : "opacity-0"
+                        selectedSlug === project.slug
+                          ? "opacity-100"
+                          : "opacity-0"
                       )}
                     />
-                    {framework.label}
+                    {project.name}
                   </CommandItem>
                 ))}
+              </CommandGroup>
+              <CommandGroup heading="新規">
+                <CommandItem
+                  value="新しいプロジェクトを作成"
+                  onSelect={() => {
+                    setSelectedSlug("");
+                    setOpen(false);
+                    router.push("/p/new");
+                  }}
+                >
+                  <Plus className="mr-2 h-4 w-4" />
+                  新しいプロジェクトを作成
+                </CommandItem>
               </CommandGroup>
             </CommandList>
           </Command>
