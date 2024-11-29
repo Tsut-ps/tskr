ac08ba1229393e2e75588f2ab9ae2ee1182512ed
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2024-11-29 13:50:21 +0900

    Change: タスクリストを期限でソートするよう変更

--- src/app/[projectSlug]/tasks/page.tsx
+++ src/app/[projectSlug]/tasks/page.tsx
@@ -47,47 +47,54 @@ export default async function Page({
             <NewTask projectSlug={projectSlug} teamId={team.id} />
           </div>
           <div className="grid gap-2">
-            {team.tasks?.map((task, index) => (
-              <Link
-                key={index}
-                href={`/${projectSlug}/tasks/${task.id}`}
-                scroll={false}
-              >
-                <Card key={index}>
-                  <CardContent className="p-5">
-                    <div className="flex items-center gap-4">
-                      <div className="grid gap-2">
-                        <p className="text-sm font-medium leading-none pt-1">
-                          {task.title}
-                        </p>
-                        <p className="text-sm text-muted-foreground">
-                          {task.start_date}→{task.due_date}
-                        </p>
-                        <div className="flex flex-wrap gap-1 pt-1">
-                          {task.tags?.map((tag, index) => (
-                            <Badge key={index} variant="secondary">
-                              {tag.name}
-                            </Badge>
-                          ))}
+            {team.tasks
+              ?.sort(
+                (a, b) => calcDaysLeft(a.due_date) - calcDaysLeft(b.due_date)
+              )
+              .map((task, index) => (
+                <Link
+                  key={index}
+                  href={`/${projectSlug}/tasks/${task.id}`}
+                  scroll={false}
+                >
+                  <Card key={index}>
+                    <CardContent className="p-5">
+                      <div className="flex items-center gap-4">
+                        <div className="grid gap-2">
+                          <p className="text-sm font-medium leading-none pt-1">
+                            {task.title}
+                          </p>
+                          <p className="text-sm text-muted-foreground">
+                            {task.start_date}→{task.due_date}
+                          </p>
+                          <div className="flex flex-wrap gap-1 pt-1">
+                            {task.tags?.map((tag, index) => (
+                              <Badge key={index} variant="secondary">
+                                {tag.name}
+                              </Badge>
+                            ))}
+                          </div>
                         </div>
+                        {
+                          <div
+                            className={cn(
+                              "ml-auto",
+                              getDateColor(task.due_date)
+                            )}
+                          >
+                            {
+                              // 残り日数が0日未満の場合は「(期限切れ)」と表示
+                              calcDaysLeft(task.due_date) < 0
+                                ? "(期限切れ)"
+                                : `残り${calcDaysLeft(task.due_date)}日`
+                            }
+                          </div>
+                        }
                       </div>
-                      {
-                        <div
-                          className={cn("ml-auto", getDateColor(task.due_date))}
-                        >
-                          {
-                            // 残り日数が0日未満の場合は「(期限切れ)」と表示
-                            calcDaysLeft(task.due_date) < 0
-                              ? "(期限切れ)"
-                              : `残り${calcDaysLeft(task.due_date)}日`
-                          }
-                        </div>
-                      }
-                    </div>
-                  </CardContent>
-                </Card>
-              </Link>
-            ))}
+                    </CardContent>
+                  </Card>
+                </Link>
+              ))}
           </div>
         </div>
       ))}
